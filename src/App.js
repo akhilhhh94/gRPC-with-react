4a6004a545b9456af0e1a94b5e35e42ae9816b8d
ak-hh <akhilhhh94@gmail.com>
2020-07-21 18:27:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const { googleRPC, reactRPC } = require("reactrpc");
+
+const requests = require("./helloworld_pb");
+const clients = require("./helloworld_grpc_web_pb.js");
+googleRPC.build(
+  requests,
+  clients,
+  "http://" + window.location.hostname + ":8080"
+);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      greeting: ''
+    };
+    const stream = this.props.Greeter.sayRepeatHello(
+      { name: "Josh", count: 10, msgType: "RepeatHelloRequest" },
+      {}
+    );
+    stream.onMessage(res => {
+      console.log(res.getMessage());
+      this.setState({
+        greeting: res.getMessage()
+  });
+    });
+    console.log(stream);
+  }
+
+  render() {
+    console.log("This is the props", this.props);
+    this.props.Health.check(
+      { service: "Greeter", msgType: "HealthCheckRequest" },
+      {},
+      (err, response) => {
+        console.log(response.getStatus());
+      }
+    );
+    return (
+      <div>
+        <h1>ReactRPC</h1>
+        <h2>{this.state.greeting}</h2>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default googleRPC.wrapper(App);
+
